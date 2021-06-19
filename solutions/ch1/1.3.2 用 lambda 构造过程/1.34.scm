e933064d4e328e187183e57c34f37a4562cd39d4
jtan <jtan.work@outlook.com>
2021-06-19 13:54:07 +0800

    Update to 1.34

---
+++ solutions/ch1/1.3.2 用 lambda 构造过程/1.34.scm
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (f g)
+  (g 2))
+
+;: (f square)
+;; 4
+
+;: (f (lambda (z) (* z (+ z 1))))
+;; 6
+
+;; (f f)
+;; (define (f g) (g 2)) <=> (define f (lambda (g) (g 2)))
+;; (f f) => (f (lambda (g) (g 2))) => ((lambda (g) (g 2)) (lambda (g) (g 2)))
+;; => 为避免混淆, 改写一下形式参数 => ((lambda ([]) ([] 2)) (lambda (g) (g 2))) 
+;; => ((lambda (g) (g 2))) 2) => (2 2)
+
+;; 因为 2 不是一个 procedure, 所以解释器报错
+;; application: not a procedure;
+;; expected a procedure that can be applied to arguments
+;; given: 2
+
+;; 这道题重点在于理解, 在定义时: 
+;; (define (function []) []) 是 (define function (lambda ([]) [])) 的语法糖
+;; 在调用时:
+;; (function x) 是 ((lambda ([]) []) x) 的语法糖
